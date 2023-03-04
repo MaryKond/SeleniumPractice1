3ad0bba3c13bc1f6aa4094ad37ab95ca45044b9c
marynakondrakhina <kondrakhina.maryna@gmail.com>
2023-03-03 23:33:27 -0500

    Terms and Policies

--- src/test/java/ErrorTest.java
+++ src/test/java/ErrorTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,10 +22,10 @@ public class ErrorTest {
         driver = SharedDriver.getWebDriver();
         driver.get(HOME_PAGE);
     }
-//    @AfterEach
-//    public void tearDown(){
-//        driver.get(HOME_PAGE);
-//    }
+    @AfterEach
+    public void tearDown(){
+        driver.get(HOME_PAGE);
+    }
 
     @Test
     public void errorMessageTest() {
@@ -60,6 +61,66 @@ public class ErrorTest {
         WebElement errorPassword = driver.findElement(By.xpath("//*[contains(text(),'Enter a combination of at least')]"));
         assertNotNull(errorPassword);
     }
+    @Test
+    public void errorGenderTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@name='websubmit']")));
+        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Kolina");
+        driver.findElement(By.xpath("//input[@name ='lastname']")).sendKeys("Mortina");
+        driver.findElement(By.xpath("//input[@name ='reg_email__']")).sendKeys("kkmorgana@gmail.com");
+        driver.findElement(By.xpath("//input[@name ='reg_email_confirmation__']")).sendKeys("kkmorgana@gmail.com");
+        driver.findElement(By.id("password_step_input")).sendKeys("Sterteo!1234!");
+        driver.findElement(By.xpath("//select[@name ='birthday_month']")).sendKeys("Jan");
+        driver.findElement(By.xpath("//select[@name ='birthday_day']")).sendKeys("25");
+        driver.findElement(By.xpath("//select[@name ='birthday_year']")).sendKeys("2000");
+        driver.findElement(By.xpath("//button[@name='websubmit']")).click();
+        pause();
+        WebElement errorGender = driver.findElement(By.xpath("//*[contains(text(),'Please choose a gender')]"));
+        assertNotNull(errorGender);
+
+    }
+
+    @Test
+    public void radioButtonsTest(){
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@name='websubmit']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[text()='Female']//following-sibling::*[@type='radio']")).click();
+        String isFemaleChecked= driver.findElement(By.xpath("//*[text()='Female']//following-sibling::*[@type='radio']")).getAttribute("checked");
+        assertEquals("true",isFemaleChecked);
+
+        driver.findElement(By.xpath("//*[text()='Male']//following-sibling::*[@type='radio']")).click();
+        String isMaleChecked= driver.findElement(By.xpath("//*[text()='Male']//following-sibling::*[@type='radio']")).getAttribute("checked");
+        assertEquals("true",isMaleChecked);
+
+        driver.findElement(By.xpath("//*[text()='Custom']//following-sibling::*[@type='radio']")).click();
+        String isCustomChecked= driver.findElement(By.xpath("//*[text()='Custom']//following-sibling::*[@type='radio']")).getAttribute("checked");
+        assertEquals("true",isCustomChecked);
+    }
+
+    @Test
+    public void PolicyTest() {
+//    Privacy Policy
+        driver.findElement(By.xpath("//a[text()='Privacy Policy']")).click();
+        String expectedURL = "https://www.facebook.com/privacy/policy/?entry_point=facebook_page_footer";
+        String actualURL = driver.getCurrentUrl();
+        assertEquals(expectedURL, actualURL);
+    }
+   @Test
+    public void TermsTest(){
+        driver.findElement(By.xpath("//a[text()='Terms']")).click();
+        String expectedURL1="https://www.facebook.com/policies_center/";
+        String actualURL1=driver.getCurrentUrl();
+        assertEquals(expectedURL1,actualURL1);
+        }
+
+
+
+
+
 // Yuri, do you use this method in your code?
     public void pause(){
         try{
